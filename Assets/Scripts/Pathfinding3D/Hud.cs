9cb7a2293cfab41bb6a946b9dd5f1cb2a4e9ad0f
Mehdi <mehdi.hasheminia@unity3d.com>
2025-09-11 22:16:56 -0700

    Adds a simple UI

---
+++ Assets/Scripts/Pathfinding3D/Hud.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class Hud : MonoBehaviour
+{
+    public GameManager m_GameManager;
+    
+    [Header("Energy")]
+    public TextMeshProUGUI m_EnergyText;
+    public PlayerAgent m_PlayerAgent; //Only for the initial value
+
+    [Header("Win/Lose")]
+    public RectTransform m_WinPanel;
+    public RectTransform m_LosePanel;
+    
+    void Start()
+    {
+        OnEnergyChanged(m_PlayerAgent.maxEnergy, m_PlayerAgent.maxEnergy);
+        
+        m_GameManager.onWin.AddListener(OnWin);
+        m_GameManager.onLose.AddListener(OnLose);
+        m_GameManager.onEnergyChanged.AddListener(OnEnergyChanged);
+    }
+
+    void OnWin()
+    {
+        m_WinPanel.gameObject.SetActive(true);
+    }
+    
+    void OnLose()
+    {
+        m_LosePanel.gameObject.SetActive(true);
+    }
+    
+    void OnEnergyChanged(int current, int max)
+    {
+        m_EnergyText.text = $"E: {current}";
+    }
+}
