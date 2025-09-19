2a5eb74546d20ae5d9c1b327f4a8cf07e3bad08a
Mehdi <mehdihasheminia@gmail.com>
2025-09-18 21:13:49 -0700

    removes unused files

--- Assets/Scripts/2D/GridBoardPreset.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GridBoardPreset", menuName = "Grid/Board Preset")]
-public class GridBoardPreset : ScriptableObject
-{
-    [TextArea(6, 20)]
-    public string json; // serialized board state
-}
